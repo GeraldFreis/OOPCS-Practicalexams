4f0110a956fe306ce316cb1b8b18ae83785a1cee
Gerald Freislich <freislichg@gmail.com>
2022-03-17 14:32:10 +1030

    commit on practicalExam1

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -13,5 +13,6 @@ int binary_to_number(int binary_digits[], int number_of_digits){
     for(int i = 0; i < number_of_digits; i++){
         decimal_number += reversed_array[i] * pow(2, i);
     }
+    
     return decimal_number;
 }
