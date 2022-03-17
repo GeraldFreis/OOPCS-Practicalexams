9d3b1a2da4330027d772e9834098fddf40a24cd9
Gerald Freislich <freislichg@gmail.com>
2022-03-17 14:32:51 +1030

    commit on PracticalExam1

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int binary_to_number(int binary_digits[], int number_of_digits){
     int reversed_array[number_of_digits];
 
-    for(int i =number_of_digits-1; i >= 0; i--){
+    for(int i = number_of_digits-1; i >= 0; i--){
         reversed_array[number_of_digits-i] = binary_digits[i];
     }
 
@@ -13,6 +13,6 @@ int binary_to_number(int binary_digits[], int number_of_digits){
     for(int i = 0; i < number_of_digits; i++){
         decimal_number += reversed_array[i] * pow(2, i);
     }
-    
+
     return decimal_number;
 }
