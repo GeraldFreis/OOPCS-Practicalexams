e80034bd625d2583f3afb9b48b7f646fbc011fc1
Gerald Freislich <freislichg@gmail.com>
2022-03-17 14:45:11 +1030

    commit on PracticalExam1

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -6,13 +6,14 @@ int binary_to_number(int binary_digits[], int number_of_digits){
     int reversed_array[number_of_digits];
 
     for(int i = number_of_digits-1; i >= 0; i--){
-        reversed_array[number_of_digits-i] = binary_digits[i];
+        reversed_array[i] = binary_digits[number_of_digits-1-i];
     }
 
-    int decimal_number = 0;
+    int decimal_number=0;
     for(int i = 0; i < number_of_digits; i++){
         decimal_number += reversed_array[i] * pow(2, i);
+        // cout << decimal_number;
     }
-
+    // cout << decimal_number << endl;
     return decimal_number;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "function-2-2.cpp"
 using namespace std;
 
 extern int binary_to_number(int [], int);
 
+// int main(int binary_digits[], int number_of_digits){
+//     binary_to_number(binary_digits, number_of_digits);
+//     return 0;
+// }
 int main(int binary_digits[], int number_of_digits){
-    binary_to_number(binary_digits, number_of_digits);
+    cout << binary_to_number(binary_digits, number_of_digits) << endl;
     return 0;
 }
