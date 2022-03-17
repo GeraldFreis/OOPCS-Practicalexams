5cffb177bf262d91fa94c21f6ae2c63d2a16dfd7
Gerald Freislich <freislichg@gmail.com>
2022-03-17 14:28:43 +1030

    commit on PracticalExam1

---
+++ function-1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printer(int array[10][10]){
+    for(int row = 0; row < 10; row++){
+        for(int column = 0; column < 10; column++){
+            if(column != 10){
+            cout << array[row][column] << " ";
+            }
+            else if(column == 10){
+                cout << array[row][column] << endl;
+            }
+        }
+    }
+}
