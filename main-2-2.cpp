cb56666237f729b3e3ab98cecde1d02546649593
Gerald Freislich <freislichg@gmail.com>
2022-03-17 14:36:28 +1030

    commit on PracticalExam1

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,6 +4,6 @@ using namespace std;
 extern int binary_to_number(int [], int);
 
 int main(int binary_digits[], int number_of_digits){
-    cout << binary_to_number(binary_digits, number_of_digits) << endl;
+    binary_to_number(binary_digits, number_of_digits);
     return 0;
 }
