af52a1f1bb487f072044a76c7c212e2c7f82bb80
Gerald Freislich <freislichg@gmail.com>
2022-03-31 14:49:13 +1030

    commit on Week12

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -7,8 +7,13 @@ int main(){
     int vals[]={-1,1,-2,2,-3,3,0,0,-5};
     int vals_length=9;
     bool res=more_positive(vals,vals_length);
-    cout << res << endl;
-
+    if(res == false){
+        cout << "vals is not more positive" << endl;
+    }
+    else{
+        cout << "vals is more positive" << endl;
+    }
+    
     int values[]={-1,-1, 1};
     int values_length=3;
     bool result=more_positive(values,values_length);
