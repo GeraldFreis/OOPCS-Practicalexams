0390b038914d625527dd8ba99171c55a1f6dda8f
Gerald Freislich <freislichg@gmail.com>
2022-03-31 15:01:17 +1030

    commit on

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -5,18 +5,18 @@ bool more_positive(int vals[], int length){
     int negative_counter = 0;
     int positive_counter = 0;
     for(int i = 0; i < length; i++){
-        if(vals[i] >= 0){
-            positive_counter += 1;
-        }
-        else{
+        if(vals[i] <=0 ){
             negative_counter += 1;
         }
+        else{
+            positive_counter += 1;
+        }
     }
-    if(positive_counter > negative_counter){
-        return true;
+    if(negative_counter >= positive_counter){
+        return false;
     }
     else{
-        return false;
+        return true;
     }
 
 }
