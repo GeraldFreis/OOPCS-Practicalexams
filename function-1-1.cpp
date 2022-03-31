202c407ab22446bf28bcbf9a3d4076ca65ccf753
Gerald Freislich <freislichg@gmail.com>
2022-03-31 14:43:36 +1030

    commit on x

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,14 +1,16 @@
  #include <iostream>
  using namespace std;
+
  int *pad_array(int *vals, int len){
      int *new_vals = new int[len+2];
      for(int i = 0; i < len; i++){
         if(i==0 || i == 1){
             new_vals[i] = vals[0];
         }
-        else if(i == len){
-            new_vals[i] = vals[len-1];
+        else if(i == len-1){
+            new_vals[i] = vals[len-2];
             new_vals[i+1] = vals[len-1];
+            new_vals[i+2] = vals[len-1];
         }
         else{
             new_vals[i] = vals[i-1];
