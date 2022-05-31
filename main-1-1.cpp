5b1a2abcefc06e386f8bb93182ed2dc52a33f938
Gerald Freislich <freislichg@gmail.com>
2022-05-31 13:47:10 +0930

    commit

--- main-1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include "Car.h"
-#include <iostream>
-using std::cout; using std::string;
-
-int main(){
-    Car car("nic", 2);
-    cout << car.get_brand() << " " << car.get_code();
-    return 0;
-};
